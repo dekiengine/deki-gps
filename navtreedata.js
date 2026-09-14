32d3559e42dedb9bf5e2a89421cae7cd76615bfc
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-14 22:02:01 +0200

    Docs for deki-gps from deki-editor f449cd3

--- navtreedata.js
+++ navtreedata.js
@@ -26,9 +26,14 @@ var NAVTREE =
 [
   [ "deki-gps", "index.html", [
     [ "Components", "index.html#autotoc_md0", null ],
-    [ "Component reference", "index.html#autotoc_md1", [
+    [ "Feature flags", "index.html#autotoc_md1", null ],
+    [ "Component reference", "index.html#autotoc_md2", [
       [ "DesktopGPSComponent", "index.html#c-desktopgpscomponent", null ],
       [ "NEO6MGPSComponent", "index.html#c-neo6mgpscomponent", null ]
+    ] ],
+    [ "Changelog", "index.html#autotoc_md3", [
+      [ "0.15.0", "index.html#autotoc_md4", null ],
+      [ "Changed", "index.html#autotoc_md5", null ]
     ] ]
   ] ]
 ];
--- navtreeindex0.js
+++ navtreeindex0.js
@@ -3,7 +3,11 @@ var NAVTREEINDEX0 =
 "index.html":[],
 "index.html#autotoc_md0":[0],
 "index.html#autotoc_md1":[1],
-"index.html#c-desktopgpscomponent":[1,0],
-"index.html#c-neo6mgpscomponent":[1,1],
+"index.html#autotoc_md2":[2],
+"index.html#autotoc_md3":[3],
+"index.html#autotoc_md4":[3,0],
+"index.html#autotoc_md5":[3,1],
+"index.html#c-desktopgpscomponent":[2,0],
+"index.html#c-neo6mgpscomponent":[2,1],
 "pages.html":[]
 };
